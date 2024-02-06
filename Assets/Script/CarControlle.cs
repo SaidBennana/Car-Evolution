c5bb6838e47a689ec7d6367caeca7ae312f39929
SaidBennana <sydbnant888@gmail.com>
2024-02-06 17:49:26 +0100

    add

--- Assets/Script/CarControlle.cs
+++ Assets/Script/CarControlle.cs
@@ -1,10 +1,12 @@
 using DG.Tweening;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CarControlle : MonoBehaviour
 {
     [SerializeField] float Speed = 30;
     [SerializeField] Transform[] Wheels;
+    [SerializeField] SwipManager swipManager;
     private void Start()
     {
         foreach (Transform item in Wheels)
@@ -22,8 +24,32 @@ public class CarControlle : MonoBehaviour
 
     void Move()
     {
-        transform.Translate(0, 0, Speed * Time.deltaTime);
+        if (swipManager.swipeRight && transform.position.x < 0.701f)
+        {
+            print("right");
+            transform.DOMoveX(0.701f, 0.5f);
+        }
+        if (swipManager.swipeLeft && transform.position.x > -0.701f)
+        {
+            print("Lift");
+            transform.DOMoveX(-0.701f, 0.5f);
+        }
 
+        transform.Translate(0, 0, Speed * Time.deltaTime);
+        limitPos();
+
+    }
+
+    void limitPos()
+    {
+        if (transform.position.x < -0.701f)
+        {
+            transform.position = new Vector3(-0.701f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > 0.701f)
+        {
+            transform.position = new Vector3(0.701f, transform.position.y, transform.position.z);
+        }
     }
 
 
